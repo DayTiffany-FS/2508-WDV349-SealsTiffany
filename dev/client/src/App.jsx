dec87bee4ca932d81f157e1ad484b88bf6b8c956
DayTiffany-FS <TKDay@student.fullsail.edu>
2026-02-09 21:08:28 -0500

    basic about band page

--- dev/client/src/App.jsx
+++ dev/client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Gigs from "./pages/Gigs";
 import Contact from "./pages/Contact";
+import AboutPage from "./pages/About"; // <-- new import
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/gigs" element={<Gigs />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<AboutPage />} /> {/* new route */}
     </Routes>
   );
 }
