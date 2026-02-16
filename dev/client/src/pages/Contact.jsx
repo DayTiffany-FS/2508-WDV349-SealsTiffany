9433aac97021b1bc73ad27b7a0617dc49bc04161
DayTiffany-FS <TKDay@student.fullsail.edu>
2026-02-15 20:05:52 -0500

    contact form created

--- dev/client/src/pages/Contact.jsx
+++ dev/client/src/pages/Contact.jsx
@@ -1,3 +1,12 @@
+import ContactForm from "../components/ContactForm";
+
 export default function Contact() {
-  return <h1>Contact Page</h1>;
+  return (
+    <main className="page">
+      <h1>Contact Off Ramp</h1>
+      <p>Want to book us for your event? Send us the details below.</p>
+
+      <ContactForm />
+    </main>
+  );
 }
